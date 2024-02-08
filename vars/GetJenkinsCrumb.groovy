2d0e0ad7e10a98fcd81bb5e8b5cf74f8167e3c7e
Dorjan Shurdhi <dorjan.shurdhi@accenture.com>
2024-02-08 18:13:04 +0100

    Update GetJenkinsCrumb.groovy

--- vars/GetJenkinsCrumb.groovy
+++ vars/GetJenkinsCrumb.groovy
@@ -4,6 +4,8 @@ def call() {
     //def crumb = sh(script: "echo \"${crumbResponse}\" | grep -oP '(?<=<crumb>).*(?=<\\/crumb>)' | awk -F: '{print \$2}'", returnStdout: true).trim()
     //return crumb
     //def url = "https://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'"
-     
+
+    sh 'pwd'
+    sh 'ls -la'
     sh(script: "sh ../scripts/getjenkinscrumb.sh")
 }
