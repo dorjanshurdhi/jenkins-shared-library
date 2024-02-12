c662f2b5f475a00fa681dc2331a7eb2440d3893f
Dorjan Shurdhi <shurdhidorian@gmail.com>
2024-02-12 16:20:23 +0100

    Update configureKubernetesPlugin.groovy

--- vars/configureKubernetesPlugin.groovy
+++ vars/configureKubernetesPlugin.groovy
@@ -2,23 +2,35 @@ import jenkins.model.*
 import jenkins.model.Jenkins
 import hudson.slaves.*
 import org.csanchez.jenkins.plugins.kubernetes.*
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import com.cloudbees.plugins.credentials.CredentialsMatchers
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
 
 def configureKubernetesPlugin(String serverUrl, String credentialsId, String namespace) {
-
+    // Ottieni l'istanza di Jenkins
     def jenkinsInstance = Jenkins.getInstance()
 
-
+    // Ottieni il plugin Kubernetes
     def kubernetesPlugin = jenkinsInstance.getPlugin('kubernetes')
 
+    // Verifica se il plugin è installato
     if (kubernetesPlugin != null) {
-
-        def cloud = new KubernetesCloud('kubernetes-test')
+        // Imposta la configurazione del plugin Kubernetes
+        def cloud = new KubernetesCloud('kubernetes')
         cloud.setServerUrl(serverUrl)
         cloud.setNamespace(namespace)
 
-        def credentials = new StringBinding(credentialsId)
-        cloud.setCredentialsId(credentialsId)
+        // Ottieni le credenziali dall'archivio di Jenkins
+        def credentials = CredentialsProvider.findCredentialById(credentialsId, StandardUsernamePasswordCredentials.class, Jenkins.getInstance(), Collections.emptyList())
 
+        if (credentials == null) {
+            println("Le credenziali con ID $credentialsId non sono state trovate.")
+            return
+        }
+
+        cloud.setCredentialsId(credentials.id)
+
+        // Aggiungi o aggiorna il cloud Kubernetes alla configurazione globale di Jenkins
         def clouds = jenkinsInstance.clouds
         boolean found = false
         for (c in clouds) {
@@ -35,6 +47,7 @@ def configureKubernetesPlugin(String serverUrl, String credentialsId, String nam
             clouds.add(cloud)
         }
 
+        // Salva le modifiche alla configurazione globale di Jenkins
         jenkinsInstance.clouds = clouds
         jenkinsInstance.save()
         
