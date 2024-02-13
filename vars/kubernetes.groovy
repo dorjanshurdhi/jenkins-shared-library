df9a9e0d2d90e390b96dd5fac04a6b98e19afcad
Dorjan Shurdhi <shurdhidorian@gmail.com>
2024-02-13 16:02:45 +0100

    add

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -19,7 +19,7 @@ def call(Map<String, Object> configMap) {
         cloud.setServerUrl(configMap['serverUrl'])
         cloud.setNamespace(configMap['namespace'])
         cloud.setUseJenkinsProxy(configMap['useJenkinsProxy'])
-        cloud.setConfig(configMap['kubeconfig'])
+        cloud.setServerCertificate(configMap['serverCertificate])
         //cloud.setDisableHTTPCertificateCheck(false)
         cloud.setJenkinsUrl(configMap['jenkinsURL'])
         cloud.setJenkinsTunnel(configMap['jenkinsTunnel'])
