72c0aeb0e79cb17e2b6691d828993ae06581cdc8
Dorjan Shurdhi <shurdhidorian@gmail.com>
2024-02-13 14:22:02 +0100

    kubernetes

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -1,32 +1,68 @@
-import org.csanchez.jenkins.plugins.kubernetes.*
 import jenkins.model.*
+import jenkins.model.Jenkins
+import hudson.slaves.*
+import org.csanchez.jenkins.plugins.kubernetes.*
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
 
-def j = Jenkins.getInstance()
+def call(Map<String, String> configMap) {
+    // Ottieni l'istanza di Jenkins
+    def jenkinsInstance = Jenkins.getInstance()
 
-def k = new KubernetesCloud(
-  'jenkins-test',
-  null,
-  'https://130.211.146.130',
-  'default',
-  'https://citest.lsst.codes/',
-  '10', 0, 0, 5
-)
-k.setSkipTlsVerify(true)
-k.setCredentialsId('ec5cf56b-71e9-4886-9f03-42934a399148')
+    // Ottieni il plugin Kubernetes
+    def kubernetesPlugin = jenkinsInstance.getPlugin('kubernetes')
 
-def p = new PodTemplate('centos:6', null)
-p.setName('centos6')
-p.setLabel('centos6-docker')
-p.setRemoteFs('/home/jenkins')
+    // Verifica se il plugin è installato
+    if (kubernetesPlugin != null) {
+        // Imposta la configurazione del plugin Kubernetes
+        def cloud = new KubernetesCloud(configMap['kubernetes-partner-name'])
+        cloud.setServerUrl(configMap['serverUrl'])
+        cloud.setNamespace(configMap['namespace'])
 
-k.addTemplate(p)
+        // Ottieni le credenziali dall'archivio di Jenkins
+      ////  def credentials = CredentialsProvider.findCredentialById(configMap['credentialsId'], StandardUsernamePasswordCredentials.class, Jenkins.getInstance(), Collections.emptyList())
+////
+      ////  if (credentials == null) {
+      ////      println("Le credenziali con ID ${configMap['credentialsId']} non sono state trovate.")
+      ////      return
+      ////  }
 
-p = new PodTemplate('lsstsqre/centos:7-docker', null)
-p.setName('centos7')
-p.setLabel('centos7-docker')
-p.setRemoteFs('/home/jenkins')
+        cloud.setCredentialsId(credentials.id)
 
-k.addTemplate(p)
+        // Configura podTemplate se presente nei dati
+        if (configMap.containsKey('podTemplate')) {
+            def podTemplateData = configMap['podTemplate']
+            def podTemplate = new PodTemplate()
+            podTemplate.setLabel(podTemplateData['label'])
+            podTemplate.setName(podTemplateData['name'])
+            podTemplate.setNamespace(podTemplateData['namespace'])
+            // Altre configurazioni di PodTemplate...
+            cloud.setTemplates([podTemplate])
+        }
 
-j.clouds.replace(k)
-j.save()
+        // Aggiungi o aggiorna il cloud Kubernetes alla configurazione globale di Jenkins
+        def clouds = jenkinsInstance.clouds
+        boolean found = false
+        for (c in clouds) {
+            if (c instanceof KubernetesCloud) {
+                if (c.name.equals('kubernetes')) {
+                    clouds.remove(c)
+                    clouds.add(cloud)
+                    found = true
+                    break
+                }
+            }
+        }
+        if (!found) {
+            clouds.add(cloud)
+        }
+
+        // Salva le modifiche alla configurazione globale di Jenkins
+        jenkinsInstance.clouds = clouds
+        jenkinsInstance.save()
+        
+        println("Plugin Kubernetes configurato con successo!")
+    } else {
+        println("Il plugin Kubernetes non è installato.")
+    }
+}
